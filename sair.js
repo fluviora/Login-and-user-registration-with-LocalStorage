77d40f02c525bc2e59775f6b78ce217893dae09b
Flúvio Rodrigues de Araujo <fluviora@gmail.com>
2021-12-07 17:24:08 -0300

    implementações finais da 1ª versão funcional
    Implementação de botão sair + token

---
+++ sair.js
@@ -0,0 +1,16 @@
+let userLogado = JSON.parse(localStorage.getItem('userLogado'))
+
+let logado = document.querySelector('#logado')
+
+logado.innerHTML = `ola ${userLogado.nome}`
+
+if(localStorage.getItem('token') == null){
+   alert('Voce precisa estar logado para acessar essa página')
+   window.location.href = '/' 
+}
+
+function sair(){
+    localStorage.removeItem('token')
+    localStorage.removeItem('userLogado')
+    window.location.href = '/'
+}
